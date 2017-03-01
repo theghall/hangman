f66712054f2841aac8002c57b591d51b798e82d5
theghall <guategary@gmail.com>
2017-03-01 14:09:58 -0500

    Add save/load of game, refactor code, rubocop changes

--- hangman.rb
+++ hangman.rb
@@ -2,37 +2,37 @@
 #
 # 20170226  GH
 #
-require 'byebug'
+
+SAVE_FILE = 'hangman.save'.freeze
 
 # All you need to play hangman
 module Hangman
   # Displays a Hangman figure for 10 misses
   class HangmanFigure
-
     attr_accessor :figure_pieces, :miss_index, :hangman
 
     def initialize
       # 8 is there twice to make sure the space
       # between the legs is included
-      @miss_index = [1,5,3,2,4,6,9,7,8,8,10]
+      @miss_index = [1, 5, 3, 2, 4, 6, 9, 7, 8, 8, 10]
 
-      @figure_pieces = "O_/|\\__/ \\_"
+      @figure_pieces = 'O_/|\\__/ \\_'
     end
 
     def display(current_guess, letter_guesses, num_misses)
       build_hangman(num_misses)
 
-      puts("_____\n")
+      puts('_____')
 
-      puts("|   |\n")
+      puts('|   |')
 
-      puts("|   #{hangman[0,1]}")
+      puts("|   #{hangman[0, 1]}")
 
-      puts("| #{hangman[1,5]}")
+      puts("| #{hangman[1, 5]}")
 
-      puts("| #{hangman[6,5]}")
+      puts("| #{hangman[6, 5]}")
 
-      puts("|_____")
+      puts('|_____')
 
       puts("Missed letters: #{letter_guesses}")
 
@@ -45,12 +45,11 @@ module Hangman
       self.hangman = ''
 
       figure_pieces.each_char.inject(0) do |index, ch|
+        break if index > figure_pieces.length
 
-       break if index > figure_pieces.length
+        self.hangman << (miss_index[index] <= num_misses ? ch : ' ')
 
-       self.hangman << (miss_index[index] <= num_misses ? ch : ' ')
-
-       index += 1
+        index += 1
       end
     end
   end
@@ -64,11 +63,10 @@ module Hangman
     end
 
     def take_turn(judge)
-
       valid_input = false
 
       until valid_input
-        puts("You can enter /help, /quit or /save at anytime.")
+        puts('You can enter /help, /quit or /save at anytime.')
 
         print("#{name}, enter guess: ")
 
@@ -78,9 +76,7 @@ module Hangman
 
         valid_command = valid_command?(guess) if guess[0].eql?('/')
 
-        valid_input = valid_command || !(guess =~ /\d|\s|\W|\t/) && guess != ""
-
-        puts('Please enter a letter, word guess, or command') unless valid_input
+        valid_input = valid_input?(guess, valid_command)
 
         judge.check_guess(guess) if valid_input && !valid_command
 
@@ -93,8 +89,16 @@ module Hangman
     def valid_command?(command)
       command = command[1..-1]
 
-      ["help", "save", "quit"].include?(command)
-    end 
+      %w(help save quit).include?(command)
+    end
+
+    def valid_input?(guess, valid_command)
+      valid_input = valid_command || !(guess =~ /\d|\s|\W|\t/) && guess != ''
+
+      puts('Please enter a letter, word guess, or command') unless valid_input
+
+      valid_input
+    end
 
     def do_command(judge, command)
       command = command[1..-1]
@@ -104,8 +108,8 @@ module Hangman
         judge.give_help
       when 'quit'
         judge.quit
-      else
-        puts("#{command} goes here")
+      when 'save'
+        judge.save(SAVE_FILE)
       end
     end
   end
@@ -118,7 +122,7 @@ module Hangman
                   :current_guess, :game_over, :num_misses, :figure, \
                   :player_quit
 
-    def initialize(figure, dictionary = '5desk.text')
+    def initialize(figure, dictionary = '5desk.txt')
       @letter_guesses = ''
 
       @player_won = false
@@ -135,14 +139,16 @@ module Hangman
     end
 
     def officiate(player)
-      set_secret_word
+      # Would be set if saved game loaded
+      set_secret_word unless secret_word
 
       figure.display(current_guess, letter_guesses, num_misses)
 
       until game_over || player_quit
         player.take_turn(self)
 
-        figure.display(current_guess, letter_guesses, num_misses)
+        figure.display(current_guess, letter_guesses, num_misses) \
+          unless player_quit
       end
 
       print_game_result(player)
@@ -150,29 +156,9 @@ module Hangman
 
     def check_guess(guess)
       if guess.length == 1
-        every_match = (0...secret_word.length).find_all \
-                      { |i| secret_word[i, 1] == guess }
-
-        if every_match.empty? && !letter_guesses.include?(guess)
-          letter_guesses << guess
-
-          self.num_misses += 1
-        end
-
-        self.game_over = every_match.empty? && \
-                         num_misses == HangmanJudge::MAX_GUESSES
-
-        every_match.each { |i| current_guess[i, 1] = guess } unless game_over
-
-        self.player_won = true if current_guess == secret_word
-
-        self.game_over = true if player_won
+        check_letter_guesses(guess)
       elsif guess.length > 1
-        self.player_won = secret_word == guess
-
-        self.num_misses += 1 unless player_won
-
-        self.game_over = true if player_won
+        check_word_guesses(guess)
       end
     end
 
@@ -180,11 +166,35 @@ module Hangman
       puts <<END_HELP
 Hangman is a guessing game in which you try to guess a secret word.
 You can guess the word a letter at a time or the entire word.  If
-your guess is incorrect then you are one step closet to being 
+your guess is incorrect then you are one step closet to being
 'hanged.'  You have 10 incorrect guesses of a letter or a word
 before you are 'hanged'.  Guessing a letter you have already incorr-
 ctly guessed will not penalize you.
+
+Press any key to continue....
 END_HELP
+
+      gets
+    end
+
+    def save(file_name)
+      begin
+        save_file = File.open(file_name, 'w')
+
+        Marshal.dump(self, save_file)
+
+        puts('Successfully saved game!')
+      rescue IOError => e
+        puts("An error occured while trying to save to #{file_name}.")
+
+        puts(e.message)
+      rescue Errno::EACCES => e
+        puts('Could not write to save file')
+      end
+
+      puts('Press any key to continue....')
+
+      gets
     end
 
     def quit
@@ -193,6 +203,33 @@ END_HELP
 
     private
 
+    def check_letter_guesses(guess)
+      every_match = (0...secret_word.length).find_all \
+                    { |i| secret_word[i, 1] == guess }
+
+      if every_match.empty? && !letter_guesses.include?(guess)
+        letter_guesses << guess
+        self.num_misses += 1
+      end
+
+      self.game_over = every_match.empty? && \
+                       num_misses == HangmanJudge::MAX_GUESSES
+
+      every_match.each { |i| current_guess[i, 1] = guess } unless game_over
+
+      self.player_won = true if current_guess == secret_word
+
+      self.game_over = true if player_won
+    end
+
+    def check_word_guesses(guess)
+      self.player_won = secret_word == guess
+
+      self.num_misses += 1 unless player_won
+
+      self.game_over = true if player_won
+    end
+
     def set_secret_word
       self.secret_word = game_dict[rand(game_dict.length)]
 
@@ -200,10 +237,15 @@ END_HELP
     end
 
     def build_game_dict(dict)
-      File.open(dict).each_with_object([]) do |word, gd|
-        word = word.chomp
+      begin
+       File.open(dict).each_with_object([]) do |word, gd|
+         word = word.chomp
 
-        gd << word if word.length >= 5 && word.length <= 12
+         gd << word if word.length >= 5 && word.length <= 12
+       end
+      rescue IOError, Errno::ENOENT
+        puts('There was an error opening the dictionary')
+        exit
       end
     end
 
